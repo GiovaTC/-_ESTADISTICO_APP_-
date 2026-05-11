fda1f071fd2e000ccdd74b7d69928c0742f1fb04
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-11 17:09:27 -0500

    commit - ConexionOracle / .

--- src/conexion/ConexionOracle.java
+++ src/conexion/ConexionOracle.java
@@ -1,4 +1,33 @@
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionOracle {
-}
+
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() {
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection conexion = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+            System.out.println("Conexion exitosa a Oracle 19c! ");
+
+            return conexion;
+        } catch (Exception e) {
+            System.out.println("Error de conexion");
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+}   
